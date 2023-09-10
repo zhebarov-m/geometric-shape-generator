e59a32fe1e6da0b88e0792c28878961a732132dd
Muhammad Zhebarov <xarcho.ev@gmail.com>
2023-09-10 23:11:11 +0300

    fix bag 'change code color'

--- src/components/CodeBlock/CodeBlock.tsx
+++ src/components/CodeBlock/CodeBlock.tsx
@@ -3,13 +3,12 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import Script from "./Script";
 
-
 const MyCodeBlock: FC = () => {
-  const { size, width, height } = useSelector(
+  const { size, width, height, color } = useSelector(
     (state: RootState) => state.square
   );
   const angles = useSelector((state: RootState) => state.radius.angles);
-  const {radius} = useSelector((state: RootState) => state.radius)
+  const { radius } = useSelector((state: RootState) => state.radius);
 
   return (
     <div
@@ -19,22 +18,34 @@ const MyCodeBlock: FC = () => {
         justifyContent: "center",
       }}
     >
-   
       <Script>
-   
         <h3>{`.square {`}</h3>
-          <p>width: <span>{width}</span>px;</p>
-          <p>height: <span>{height}</span>px;</p>
-          <p>background-color: <span>white</span>;</p>
-          {radius ? (
-        <p>border-radius: <span>{radius}px;</span></p>
-      ) : (
-        <p>border-radius: <span>{angles[0]}</span>px <span>{angles[1]}</span>px <span>{angles[2]}</span>px <span>{angles[3]}</span>px;</p>
-      )}
-          <p>transform: scale(<span>{size}</span>)</p>
-          <p>transform:: rotate(<span>{45}</span>deg)</p>
+        <p>
+          width: <span>{width}</span>px;
+        </p>
+        <p>
+          height: <span>{height}</span>px;
+        </p>
+        <p>
+          background-color: <span>{color}</span>;
+        </p>
+        {radius ? (
+          <p>
+            border-radius: <span>{radius}px;</span>
+          </p>
+        ) : (
+          <p>
+            border-radius: <span>{angles[0]}</span>px <span>{angles[1]}</span>px{" "}
+            <span>{angles[2]}</span>px <span>{angles[3]}</span>px;
+          </p>
+        )}
+        <p>
+          transform: scale(<span>{size}</span>)
+        </p>
+        <p>
+          transform:: rotate(<span>{45}</span>deg)
+        </p>
         <h3>{`}`}</h3>
-
       </Script>
     </div>
   );
